c23466743ee8bccbc1b1e449312d0aca6e9793ea
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-10-17 08:23:06 +0400

    Remove some changes

--- main/views.py
+++ main/views.py
@@ -5,19 +5,12 @@ from django.shortcuts import render
 # Create your views here.
 
 form_html = """
-<html>
-<head>
-	<title>Shopping list</title>
-</head>
-<body>
 	<form method="GET">
 		<h2>Add a food</h2>
 		<input type="text" name="food" value="" placeholder="What to buy?">
 		%s
 		<input type="submit">
 	</form>
-</body>
-</html>
 """
 
 hidden_html = """
@@ -34,19 +27,20 @@ shopping_list_html = """
 """
 
 class ShoppingView(View):
-	# def get(self, request):
-	# 	# from pdb import set_trace; set_trace()
-	# 	output = form_html
-	# 	hidden_output = ''
-	# 	shopping_list_items = ''
-	# 	for item in request.GET.getlist('food'):
-	# 		hidden_output += hidden_html % item
-	# 		shopping_list_items += '<li>%s</li>' % item
-		
-	# 	return HttpResponse(output % hidden_output + shopping_list_html%shopping_list_items)
-
 	def get(self, request):
-		return HttpResponse(render(request, 'form.html', {'food_values': request.GET.getlist('food')}))
+		output = form_html
+		
+		return HttpResponse(output)
+
 
 	def post(self, request):
 		return HttpResponse('Hello Post')
+
+
+
+
+
+
+
+	# def get(self, request):
+	# 	return HttpResponse(render(request, 'form.html', {'food_values': request.GET.getlist('food')}))
