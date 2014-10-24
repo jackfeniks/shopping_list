39b1462468e6bf5e2aac5adbc124533be61b8ba6
unknown <elp1@k101.lyceum.local>
2014-10-24 16:08:54 +0300

    Initial commit

--- main/views.py
+++ main/views.py
@@ -4,33 +4,62 @@ from django.views.generic import View
 from django.shortcuts import render
 # Create your views here.
 
-form_html = """
-	<form method="GET">
-		<h2>Add a food</h2>
-		<input type="text" name="food" value="" placeholder="What to buy?">
-		%s
-		<input type="submit">
-	</form>
-"""
+# form_html = """
+# 	<form method="GET">
+# 		<h2>Add a food</h2>
+# 		<input type="text" name="animal1" value="" placeholder="What to buy?">
+# 		%s
+# 		<input type="submit">
+# 	</form>
+# """
 
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
+# hidden_html = """
+# <input type="hidden" name="food" value="%s">
+# """
 
-shopping_list_html = """
-<br>
-<br>
-<h2> Shopping list</h2>
-<ul>
-%s
-</ul>
-"""
+# shopping_list_html = """
+# <br>
+# <br>
+# <h2> Shopping list</h2>
+# <ul>
+# %s
+# </ul>
+# """
 
 class ShoppingView(View):
-	def get(self, request):
-		output = form_html
+	# def get(self, request):
+	# 	output = form_html
 		
-		return HttpResponse(output)
+	# 	return HttpResponse(output)
+
+
+	def get(self, request):
+		# items = ['q1', 'q2', 'string', 'char', 'llll']
+		items = range(1,60)
+		result = []
+
+		for item in items:
+			if item % 3 == 0:
+				result.append('fizz')
+			else:
+				result.append(item)
+				continue
+
+			if item % 5 == 0:
+				result.append('buzz')
+			else:
+				result.append(item)
+				continue
+			if item % 15 == 0:
+				result.append('fizzbuzz')
+			else:
+				result.append(item)
+
+			# fizz buzz fizzbuzz
+		return HttpResponse(render(request, 'shopping_list.html', 
+			{'name': request.GET['name'],
+			'items': result}))
+			# 'animal2': request.GET.get('animal2') }))
 
 
 	def post(self, request):
